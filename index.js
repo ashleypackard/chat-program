5baf31e81f804dc90cd4efb4873ca91226b86299
Ashley <Ashley>
2015-11-01 17:16:08 -0800

    modify to run on heroku

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 var express = require('express');
 var moment = require('moment');
+var socketio = require('socket.io');
+
 var app = express();
+var io = socketio.listen(app);
 var http = require('http').Server(app);
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
 var users = {};
 
-server.listen(process.env.PORT || 3000);
-
 app.use('/assets', express.static(__dirname + '/assets'));
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function(socket){
 	// on connection we want to show them who's in the chat
-	io.emit('updateUserList', users);
+	io.sockets.emit('updateUserList', users);
 
 	socket.on('createUsername', function(username){
 		if(username != '') {
 			socket.username = username;
 			users[username] = username;
 			// add chat message alerting users
-			io.emit('alertUsers', username + " has joined the room", 'user-joined', moment().format('h:mm:ss a'));
+			io.sockets.emit('alertUsers', username + " has joined the room", 'user-joined', moment().format('h:mm:ss a'));
 			// add user to users list
-			io.emit('updateUserList', users);
+			io.sockets.emit('updateUserList', users);
 		}
 	});
 
 	socket.on('sendChatMessage', function(msg){
 		if(msg != '') {
-			io.emit('sendChatMessage', socket.username, msg, moment().format('h:mm:ss a'));
+			io.sockets.emit('sendChatMessage', socket.username, msg, moment().format('h:mm:ss a'));
 		}
 	});
 
